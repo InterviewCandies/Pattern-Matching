6df6e66088df94d1a35abb5ee75700a0d90b77b9
1712162 <1712162@student.hcmus.edu.vn>
2020-10-11 17:46:07 +0700

    Add Naive approach

---
+++ Naive.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+#include<vector>
+using namespace std;
+bool IsMatched(string & a, string & b) {
+    for (int i = 0; i < a.size(); i++)
+        if(a[i] != b[i]) return false;
+    return true;
+}
+vector<int> Naive(string & s, string & t) {
+    vector<int> pos;
+    for (int i = 0; i <= s.size() - t.size(); i++) {
+        string tmp = s.substr(i, t.size());
+        if (IsMatched(tmp, t)) pos.push_back(i);
+    }
+    return pos;
+}
+int main() {
+    string s = "abdabcabcadabdabcsdsdasfsfasaabdabc";
+    string t = "abdabc";
+    auto ans = Naive(s, t);
+    for(auto it : ans) cout << it << " ";
+    return 0;
+}
